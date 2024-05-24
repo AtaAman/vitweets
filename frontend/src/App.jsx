fee753c35120208259656366ce63ff07f0ef16a3
AmanAta <atamuhammadaman@gmail.com>
2024-05-24 17:58:57 +0530

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-4xl text-white text-center
+      pt-10">vitweets</h1>
+    </>
+  );
+}
+
+export default App;
